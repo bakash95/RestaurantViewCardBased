9068bba4d087431c9e1f312ca9f0a85b7a4294d9
Akash <akash@akash.com>
2020-05-03 23:37:13 +0530

    mock server update

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const favicon = require('express-favicon');
 const compression = require('compression')
 const path = require('path');
+let bodyParser = require('body-parser');
+let fs = require('fs');
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(compression())
@@ -13,7 +15,7 @@ app.use(function (request, response, nextCall) {
   if (request.secure) {
     nextCall();
   } else {
-      response.redirect('https://' + request.headers.host + request.url);
+    response.redirect('https://' + request.headers.host + request.url);
   }
 })
 app.get('/ping', function (req, res) {
@@ -22,4 +24,21 @@ app.get('/ping', function (req, res) {
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/initData',
+  (req, res) => {
+    let menuPath = path.join(__dirname, 'build/server', 'initData.json');
+    let menuPathFile = fs.readFileSync(menuPath);
+    res.setHeader('Content-Type', 'application/json');
+    res.write(menuPathFile)
+    res.end()
+  })
+
 app.listen(port);
